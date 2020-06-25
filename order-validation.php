1a93859f6f955827edc01eb59b88c921169de47c
arora-akash <33399484+arora-akash@users.noreply.github.com>
2020-06-25 13:14:26 +0530

    Update order-validation.php

--- order-validation.php
+++ order-validation.php
@@ -15,40 +15,42 @@
 	session_start();
 	if (isset($_SESSION['loggedincust']) && $_SESSION['loggedincust'] == true) 
 	{
+		$txt= $_GET['id'];
+
+		$sql="SELECT r_name FROM menu WHERE id='$txt'";
+		$result = mysqli_query($check,$sql);
+		$row1 = mysqli_fetch_array($result);
+	
+		$sql1="SELECT dish FROM menu WHERE id='$txt'";
+		$res = mysqli_query($check,$sql1);
+		$row2 = mysqli_fetch_array($res);
+
+		$sql2 = "SELECT price FROM menu WHERE id='$txt'";
+		$que = mysqli_query($check,$sql2);
+		$row3 = mysqli_fetch_array($que);
+	
+		$now = new DateTime();
+    		$now->setTimezone(new DateTimeZone('Asia/Kolkata'));
+    		$x =  $now->format('Y-m-d H:i:s');
+
+		$sql97="SELECT c_name FROM customer WHERE c_mobile='".$_SESSION['username']."' ";
+		$result97 = mysqli_query($check,$sql97);
+		$row197 = mysqli_fetch_array($result97);
+
+		$sql3 = " INSERT INTO orders VALUES('$row1[0]','$row2[0]','$row3[0]','$row197[0]','$x','".$_SESSION['username']."') ";
+		$execute = mysqli_query($check,$sql3);
+
+		if($execute)
+		{
+			echo '<script>alert("ORDER PLACED")</script>';
+			echo '<script>window.location.href="menu.php"</script>';
+		}
 	}
+
 	else
 	{
 		echo '<script>alert("Please log in first to see this page.")</script>';
     	echo '<script>window.location.href="foodshala.html"</script>';	
 	}
-	$txt= $_GET['id'];
-
-	$sql="SELECT r_name FROM menu WHERE id='$txt'";
-	$result = mysqli_query($check,$sql);
-	$row1 = mysqli_fetch_array($result);
 	
-	$sql1="SELECT dish FROM menu WHERE id='$txt'";
-	$res = mysqli_query($check,$sql1);
-	$row2 = mysqli_fetch_array($res);
-
-	$sql2 = "SELECT price FROM menu WHERE id='$txt'";
-	$que = mysqli_query($check,$sql2);
-	$row3 = mysqli_fetch_array($que);
-	
-	$now = new DateTime();
-    	$now->setTimezone(new DateTimeZone('Asia/Kolkata'));
-    	$x =  $now->format('Y-m-d H:i:s');
-
-	$sql97="SELECT c_name FROM customer WHERE c_mobile='".$_SESSION['username']."' ";
-	$result97 = mysqli_query($check,$sql97);
-	$row197 = mysqli_fetch_array($result97);
-
-	$sql3 = " INSERT INTO orders VALUES('$row1[0]','$row2[0]','$row3[0]','$row197[0]','$x','".$_SESSION['username']."') ";
-	$execute = mysqli_query($check,$sql3);
-
-	if($execute)
-	{
-		echo '<script>alert("ORDER PLACED")</script>';
-		echo '<script>window.location.href="menu.php"</script>';
-	}
 ?>
